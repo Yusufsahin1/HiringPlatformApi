3a473715b68cb834a50c2fe8cb9e05ecaefa62b7
Yusufsahin1 <ys.sahinyusuf@gmail.com>
2025-03-21 03:37:45 +0300

    Initial commit

---
+++ src/main/java/com/yusufsahin/hiring_platform_api/HiringPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.yusufsahin.hiring_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringPlatformApiApplication.class, args);
+	}
+
+}
